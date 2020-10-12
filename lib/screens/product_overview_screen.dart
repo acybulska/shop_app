d5454ee8eb2cfcb1ec773b5289fe9607de0b965e
Aleksandra Lisa Cybulska <aleksandra.cybulska@aequitas-group.pl>
2020-10-12 14:11:30 +0200

    Show favourite items

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,16 +1,52 @@
 import 'package:flutter/material.dart';
 
- import '../widgets/products_grid.dart';
+import '../widgets/products_grid.dart';
+import '../providers/products.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
+  @override
+  _ProductOverviewScreenState createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("My Shop"),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text("Only favorites"),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text("Show all"),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -5,10 +5,14 @@ import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
@@ -19,11 +23,7 @@ class ProductsGrid extends StatelessWidget {
       ),
       itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
         value: products[i],
-        child: ProductItem(
-            // products[i].id,
-            // products[i].title,
-            // products[i].imageUrl,
-            ),
+        child: ProductItem(),
       ),
       itemCount: products.length,
       padding: const EdgeInsets.all(10.0),
